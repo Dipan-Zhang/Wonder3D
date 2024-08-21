dde63b657a0c9dc438df25834106143be19815b2
Anran Zhang <anran.zhang@tum.de>
2024-08-21 11:43:05 +0200

    + new load function, + new initialization of rotation part, + limited RX

--- NeuS/pose_optimizer.py
+++ NeuS/pose_optimizer.py
@@ -1,13 +1,8 @@
 import torch
 import numpy as np
 import open3d as o3d
-import torch.nn.functional as F
-import torch.nn as nn
-import math
 import os
 import cv2
-from PIL import Image
-import ipdb
 import json
 from pyhocon import ConfigFactory
 
@@ -21,7 +16,6 @@ from scipy.spatial.transform import Rotation as R
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 def load_checkpoint(checkpoint_fname):
-    # read network config
     conf_path = './confs/wmask.conf'
     f = open(conf_path)
     conf_text = f.read()
@@ -54,9 +48,6 @@ def visualize_points(points, colors=None):
         pcd.colors = o3d.utility.Vector3dVector(colors)
     return pcd
 
-def visualize_point_cloud(pcd):
-    # Visualize the point cloud
-    o3d.visualization.draw_geometries([pcd])
 
 def backproject(depth, intrinsics, instance_mask, NOCS_convention=True):
     intrinsics_inv = np.linalg.inv(intrinsics)
@@ -93,6 +84,7 @@ def backproject(depth, intrinsics, instance_mask, NOCS_convention=True):
         pts[:, 2] = -pts[:, 2]
     return pts, idxs
 
+
 def read_dataset_single_img(dataset_path, frame_idx):
     # image file name
     rgb_image_path = os.path.join(dataset_path, 'rgbsyn_init_calib/{:06d}.png'.format(frame_idx))
@@ -117,93 +109,104 @@ def read_dataset_single_img(dataset_path, frame_idx):
 
     return rgb_img, depth_img, T_co, T_oc, camera_intrinsic
 
-def save_results(Trans_mat_list):
 
-    # for index, view in enumerate(Trans_mat_list):
-    with open("./optimization_result.json", "w") as outfile: 
-        json.dump(Trans_mat_list, outfile,indent=1)
+def save_dict(save_path, dict):
+    """a dict to a json file"""
+    with open(save_path, "w") as outfile: 
+        json.dump(dict, outfile,indent=1)
+
+def Rz_rand():
+    """create a random rotation matrix around z axis"""
+    theta = np.random.rand()*2*np.pi
+    R_z = np.array([[np.cos(theta), -np.sin(theta), 0],
+                    [np.sin(theta), np.cos(theta), 0],
+                    [0, 0, 1]])
+    return R_z
+
+def Ry_rand():
+    """create a random rotation matrix around y axis"""
+    theta = np.random.rand()*2*np.pi
+    R_y = np.array([[np.cos(theta), 0, np.sin(theta)],
+                    [0, 1, 0],
+                    [-np.sin(theta), 0, np.cos(theta)]])
+    return R_y
+
+def Rx_rand():
+    """create a random rotation matrix around x axis"""
+    theta = np.random.rand()*2*np.pi
+    R_x = np.array([[1, 0, 0],
+                    [0, np.cos(theta), -np.sin(theta)],
+                    [0, np.sin(theta), np.cos(theta)]])
+    return R_x
+
+def Rx_rand_limited():
+    """create a random rotation matrix around x axis with range 0-PI"""
+    theta = np.random.rand()*np.pi
+    R_x = np.array([[1, 0, 0],
+                    [0, np.cos(theta), -np.sin(theta)],
+                    [0, np.sin(theta), np.cos(theta)]])
+    return R_x
+
+def sample_T_oc_init_list(n,translation):
+    T_list = []
+    for _ in range(n):
+        T_co_init = np.eye(4)
+        T_co_init[:3, :3] = Rx_rand_limited()
+        T_co_init[:3, 3] = translation 
+        T_list.append(T_co_init)
+    return T_list
+
+
+def rank_T_init(sdf_network,pts,T_init_list):
+    """rank the sdf value of the points based on the rotation matrix"""
+    sdf_list = []
+    for T_init in T_init_list:
+        pts_in_obj = pts@ T_init[:3,:3].T + T_init[:3,3]
+        sdf = sdf_network.sdf(torch.tensor(pts_in_obj).to(device).float())
+        sdf_list.append(sdf.mean())
+    sdf_list = torch.stack(sdf_list)
+    min_idx =  torch.argmin(torch.abs(sdf_list))
+
+    return T_init_list[min_idx],min_idx
+
 
 def owl():
+    """case for rendered dataset"""
     obj_name = 'owl'
     ckpt_path = './exp/neus/'+ obj_name +'/checkpoints/ckpt_005000.pth'
     sdf_network = load_checkpoint(ckpt_path)
+    mesh_path = os.path.join(dataset_path, 'owl.ply')
+    mesh = o3d.io.read_triangle_mesh(mesh_path)
     
-    config_path = './confs/optimizer.json' # into args
-    configs = get_configs(config_path)
-
-    dataset_path = './data/pose_estimation/'
+    dataset_path = '../customized_data/owl_rendered/pose_estimation/'
     frame_idx = 16
     rgb_img, depth_img, T_co, T_oc, camera_intrinsic = read_dataset_single_img(dataset_path,frame_idx)
 
     pts,_ = backproject(depth_img,camera_intrinsic, depth_img>0, NOCS_convention=False) # (16768, 3)
 
-    # better downsample
     selected_indices = np.random.choice(pts.shape[0], 1000, replace=False) 
     pts = pts[selected_indices]
-    mesh_path = os.path.join(dataset_path, 'owl.ply')
-    mesh = o3d.io.read_triangle_mesh(mesh_path)
 
-    optimizer = Optimizer(sdf_network,configs)
+    translation_guess = pts.mean(axis=0)
+    T_co_init_list = sample_T_oc_init_list(10,translation_guess)
+    T_co_init_ranked, _ = rank_T_init(sdf_network,pts,T_co_init_list)
 
-    optimization_results = {}
-    # batch process??
-    for id in range(2):
-        T_co_noised = T_co.copy()
-        T_co_noised[:3, 3] =  T_co_noised[:3, 3] + np.random.rand(3)*0.3
-        R_noised = R.from_euler('xyz', np.random.rand(3)*1, degrees=False)
-        T_co_noised[:3, :3] =  T_co_noised[:3, :3] @ R_noised.as_matrix()
-
-        T_co_optimized = optimizer.estimate_pose_cam_obj(T_co_noised,scale=1.0,pts=pts) 
-        T_co_optimized = T_co_optimized.detach().numpy()
-        
-        # get the inverse of the transformation
-        T_oc_noised = np.linalg.inv(T_co_noised)
-        T_oc_optimized = np.linalg.inv(T_co_optimized)
-
-        optimization_results[id] = {'T_oc_optimized': T_co_optimized.tolist(),
-                                   'T_oc_noised': T_oc_noised.tolist(),
-                                   'T_oc_gt': T_oc.tolist()
-                                    }
-        
-        pts_in_obj_gt = pts@ T_oc[:3,:3].T+ T_oc[:3,3]
-        pts_in_obj_noised = pts@ T_oc_noised[:3,:3].T + T_oc_noised[:3,3]
-        pts_in_obj_optimized = pts@ T_oc_optimized[:3,:3].T + T_oc_optimized[:3,3]
-        
-        pcd_gt = visualize_points(pts_in_obj_gt)
-        pcd_optimized = visualize_points(pts_in_obj_optimized)
-        pcd_noised = visualize_points(pts_in_obj_noised)
-
-        pcd_gt.paint_uniform_color([0, 1, 0]) # in green
-        pcd_optimized.paint_uniform_color([0, 0, 1]) # in blue
-        pcd_noised.paint_uniform_color([1, 0, 0]) # in red
-
-        # o3d.visualization.draw_geometries([mesh,pcd_noised])
-        # o3d.visualization.draw_geometries([mesh,pcd_optimized])
-        o3d.visualization.draw([pcd_gt,pcd_optimized,pcd_noised, mesh])
-        print(f'peak memory used after iteration {id} {torch.cuda.max_memory_allocated()*1e-9} GB')
-    
-    save_results(optimization_results)
-
-    print("optimization done")
+    return pts, mesh, sdf_network, T_co, T_co_init_ranked
 
 
-if __name__ == "__main__":
+def cup_hz():
     obj_name = 'cup_hz'
     ckpt_path = './exp/neus/'+ obj_name +'/checkpoints/ckpt_003000.pth'
     sdf_network = load_checkpoint(ckpt_path)
     mesh_path = '/home/stud/zanr/code/tmp/Wonder3D/NeuS/exp/neus/cup_hz/meshes/cup_hz_3000.glb'
     mesh = o3d.io.read_triangle_mesh(mesh_path)
     
-    config_path = './confs/optimizer.json' # into args
-    configs = get_configs(config_path)
-
     # load dataset
-    dataset_path = '../customized_data/2023-12-05-17-44-48/'
+    dataset_path = '../customized_data/cpu_hz/'
     rgb_image_path = os.path.join(dataset_path, 'object_rgba.png')
     mask_image_path = os.path.join(dataset_path, 'object_pose/0_mask.png')
     depth_image_path = os.path.join(dataset_path, 'depth/000001.png')
-    
-    # read image
+
     rgba_image = cv2.imread(rgb_image_path, -1)
     rgb_image = rgba_image[..., :3][...,[2,1,0]]
     mask_image = cv2.imread(mask_image_path, -1)
@@ -216,62 +219,147 @@ if __name__ == "__main__":
                 [0, 0, 1],
             ]
         )
+    
+    # quasi gt
     T_co_gt = np.loadtxt(os.path.join(dataset_path, 'object_pose/0_Rt_cam_obj.txt')).reshape(4, 4)
     rot_x90 = np.array([[1, 0, 0, 0], [0, 0, -1, 0], [0, 1, 0, 0], [0, 0, 0, 1]])
     T_co_gt = T_co_gt @ rot_x90.T
 
     pts,_ = backproject(depth_img,camera_intrinsic, mask_image, NOCS_convention=False)
 
-    # better downsample
     selected_indices = np.random.choice(pts.shape[0], 1000, replace=False) 
     pts = pts[selected_indices]
 
+    translation_guess = pts.mean(axis=0)
+    T_co_init_list = sample_T_oc_init_list(10,translation_guess)
+    T_co_init_ranked, _ = rank_T_init(sdf_network,pts,T_co_init_list)
+    
+    return pts, mesh, sdf_network, T_co_gt, T_co_init_ranked
+
+
+def load_kinect_dataset(case_name,mask_id=0):
+    """load self recorded kinect dataset"""
+    ckpt_path = './exp/neus/'+ case_name +'/checkpoints/ckpt_003000.pth'
+    sdf_network = load_checkpoint(ckpt_path)
+
+    mesh_path = f'/home/stud/zanr/code/tmp/Wonder3D/NeuS/exp/neus/{case_name}/meshes/{case_name}_3000.glb'
+    mesh = o3d.io.read_triangle_mesh(mesh_path)
+
+    dataset_path = '../customized_data/table_top/'
+    rgb_image_path = os.path.join(dataset_path, 'object_rgba_1.png')
+    # depth_image_path = os.path.join(dataset_path, 'object_depth.png')
+    mask_image_path = os.path.join(dataset_path, f'masks/mask_{mask_id}.png')
+    depth_image_path = os.path.join(dataset_path, 'depth/00001.png')
+    background_mask_path = os.path.join(dataset_path, 'desk_mask.png')
+    # depth_image_path = '/home/stud/zanr/code/tmp/Wonder3D/customized_data/2023-12-05-17-44-48/object_depth_test.png'
+
+    # read image
+    rgba_image = cv2.imread(rgb_image_path, -1)
+    rgb_image = rgba_image[..., :3][...,[2,1,0]]
+    mask_image = cv2.imread(mask_image_path, -1)
+    desk_mask = cv2.imread(background_mask_path, -1)
+
+    depth_img = cv2.imread(depth_image_path, -1)/1000 
+    print(f'The depth mean is {depth_img.mean()}')
+
+    camera_intrinsic = np.array(
+            [
+                [608.7960205078125, 0, 632.1019287109375],
+                [0, 608.9515380859375, 365.985595703125],
+                [0, 0, 1],
+            ]
+        )
+    pts, _ = backproject(depth_img, camera_intrinsic,mask_image, NOCS_convention=False)
+    selected_indices = np.random.choice(pts.shape[0], 1000, replace=False) 
+    pts = pts[selected_indices]
+
+    pcd = visualize_points(pts)
+    pcd_remove_outlier, _ = pcd.remove_radius_outlier(10,0.2)
+    pcd_remove_outlier, _ = pcd_remove_outlier.remove_statistical_outlier(nb_neighbors=40,std_ratio=2.0)
+
+    print("this case does not have ground truth")
+    T_co_gt = np.eye(4)
+
+    # generate bunch of initial guess and rank them
+    translation_guess = pts.mean(axis=0)
+    T_co_init_list = sample_T_oc_init_list(10,translation_guess)
+    T_co_init_ranked, _ = rank_T_init(sdf_network,pts,T_co_init_list)
+
+    return np.asarray(pcd_remove_outlier.points), mesh, sdf_network, T_co_gt,T_co_init_ranked
+
+
+def prepare_optimization(case_name):
+    if case_name =='owl':
+        #rendered dataset
+        return owl()
+    elif case_name == 'cup_hz':
+        # real dataset from hz
+        return cup_hz()
+    elif case_name == 'object_rgba_8':
+        # real dataset from ar
+        mask_id = case_name[-1]
+        return load_kinect_dataset(case_name,mask_id)
+    else:
+        raise NotImplementedError
+    
+
+if __name__ == "__main__":
+    case_name = 'object_rgba_8'
+    pts, mesh, sdf_network, T_co_gt,T_co_init = prepare_optimization(case_name)
+
+    config_path = './confs/optimizer.json' # into args
+    configs = get_configs(config_path)
     optimizer = Optimizer(sdf_network,configs)
-    scale = 15.0
-    optimization_results = {}
-
     
-    for id in range(2):
-        T_co_noised = T_co_gt.copy()
-        T_co_noised[:3, 3] =  T_co_noised[:3, 3] + np.random.rand(3)*0.3
-        R_noised = R.from_euler('xyz', np.random.rand(3)*0.3, degrees=False)
-        T_co_noised[:3, :3] =  T_co_noised[:3, :3] @ R_noised.as_matrix()
+    # estimate scale
+    bounding_box_mesh = mesh.get_axis_aligned_bounding_box()
+    diagonal_length_mesh = np.linalg.norm(bounding_box_mesh.get_max_bound() - bounding_box_mesh.get_min_bound())
+    diagonal_length_pts = np.linalg.norm(pts.max(axis=0) - pts.min(axis=0))
+    scale = diagonal_length_mesh/diagonal_length_pts
+    print(f'Estimate initial scale {scale}')
 
-        T_co_optimized = optimizer.estimate_pose_cam_obj(T_co_noised,scale=1/scale,pts=pts)  # scale should be inverse with T_co
-        T_co_optimized = T_co_optimized.detach().numpy()
-        
-        # get the inverse of the transformation
-        T_oc_gt = np.linalg.inv(T_co_gt)
-        T_oc_noised = np.linalg.inv(T_co_noised)
-        T_oc_optimized = np.linalg.inv(T_co_optimized)
-        print(repr(T_oc_optimized))
+    # feed the ranked initial guess to the optimizer
+    T_co_optimized = optimizer.estimate_pose_cam_obj(T_co_init,scale=1/scale,pts=pts)  # scale should be inverse with T_co
+    T_co_optimized = T_co_optimized.detach().numpy()
 
-        optimization_results[id] = {'T_oc_optimized': T_co_optimized.tolist(),
-                                   'T_oc_noised': T_oc_noised.tolist(),
-                                   'T_oc_gt': T_oc_gt.tolist()
-                                    }
-        
-        pts_in_obj_gt = pts@ T_oc_gt[:3,:3].T+ T_oc_gt[:3,3]
-        pts_in_obj_gt_scale = pts_in_obj_gt*scale
-        pts_in_obj_noised = pts@ T_oc_noised[:3,:3].T + T_oc_noised[:3,3]
-        pts_in_obj_noised_scale = pts_in_obj_noised*scale
-        pts_in_obj_optimized = pts@ T_oc_optimized[:3,:3].T + T_oc_optimized[:3,3]
-        pts_in_obj_optimized_scale = pts_in_obj_optimized*scale
-        
-        pcd_gt = visualize_points(pts_in_obj_gt_scale)
-        pcd_noised = visualize_points(pts_in_obj_noised_scale)
-        pcd_optimized = visualize_points(pts_in_obj_optimized_scale)
 
-        pcd_gt.paint_uniform_color([0, 1, 0]) # in green
-        pcd_optimized.paint_uniform_color([0, 0, 1]) # in blue
-        pcd_noised.paint_uniform_color([1, 0, 0]) # in red
+    # get the inverse of the transformation
+    T_oc_gt = np.linalg.inv(T_co_gt)
+    T_oc_init = np.linalg.inv(T_co_init)
+    T_oc_optimized = np.linalg.inv(T_co_optimized)
 
-        # o3d.visualization.draw_geometries([mesh,pcd_noised])
-        # o3d.visualization.draw_geometries([mesh,pcd_optimized])
-        o3d.visualization.draw([pcd_gt,pcd_optimized,pcd_noised, mesh])
+    # transfer to obj frame        
+    pts_in_obj_gt = pts@ T_oc_gt[:3,:3].T+ T_oc_gt[:3,3]
+    pts_in_obj_gt_scaled = pts_in_obj_gt*scale
+    pts_in_obj_init = pts@ T_oc_init[:3,:3].T + T_oc_init[:3,3]
+    pts_in_obj_init_scaled = pts_in_obj_init*scale
+    pts_in_obj_optimized = pts@ T_oc_optimized[:3,:3].T + T_oc_optimized[:3,3]
+    pts_in_obj_optimized_scaled = pts_in_obj_optimized*scale 
 
-        print(f'peak memory used after iteration {id} {torch.cuda.max_memory_allocated()*1e-9} GB')
-    
-    save_results(optimization_results)
+    axis = o3d.geometry.TriangleMesh.create_coordinate_frame()
+    pcd_gt = visualize_points(pts_in_obj_gt_scaled)
+    pcd_init = visualize_points(pts_in_obj_init_scaled)
+    pcd_optimized = visualize_points(pts_in_obj_optimized_scaled)
+
+    pcd_gt.paint_uniform_color([0, 1, 0]) # in green
+    pcd_optimized.paint_uniform_color([0, 0, 1]) # in blue
+    pcd_init.paint_uniform_color([1, 0, 0]) # in red
+
+    # o3d.visualization.draw_geometries([mesh,pcd_init])
+    o3d.visualization.draw_geometries([mesh,pcd_optimized])
+    # o3d.visualization.draw([pcd_gt,pcd_optimized,pcd_init, mesh,axis])
+
+    # o3d.visualization.capture_screen_image('./screen.jpg')
+    # record the results
+    optimization_results = {'T_oc_optimized': T_oc_optimized.tolist(),
+                            'T_oc_init': T_oc_init.tolist(),
+                            'T_oc_gt': T_oc_gt.tolist()
+                            }
 
     print("optimization done")
+    print(f'peak memory used: {torch.cuda.max_memory_allocated()*1e-9} GB')
+
+    optimization_path = './optimization_results.json'
+    save_dict(optimization_path,optimization_results)
+    print(f'saved results to {optimization_path}')
+
