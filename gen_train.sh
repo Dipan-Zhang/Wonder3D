c716f8aa817db4bdaf6a0671c9b57c8b590f94e0
Anran Zhang <anran.zhang@tum.de>
2024-10-06 10:53:56 +0200

    resolved confilcits

--- gen_train.sh
+++ gen_train.sh
@@ -2,15 +2,21 @@
 
 # Function to display usage information
 usage() {
-    echo "Usage: $0 [--neus_only] CASE_NAME1 [CASE_NAME2 ...]"
+    echo "Usage: $0 [--neus_only] [--sn_est] CASE_NAME1 [CASE_NAME2 ...]"
     echo "  --neus_only  Only run the second command (exp_runner.py)"
     exit 1
 }
 
 # Parse optional --neus_only flag
 NEUS_ONLY=false
+SN_EST=false
 if [ "$1" == "--neus_only" ]; then
     NEUS_ONLY=true
+    shift # Remove the flag from the arguments
+fi
+
+if [ "$1" == "--sn_est" ]; then
+    SN_EST=true
     shift # Remove the flag from the arguments
 fi
 
@@ -40,16 +46,18 @@ do
     DATASET_NAME="${CASE_NAME%_*}"  # Everything before the last underscore (e.g., table_1_far)
     OBJ_NUMBER="${CASE_NAME##*_}" # The part after the last underscore (e.g., 1)
 
-    # Check if the directory exists
-    if [ ! -d "../instant-nsr-pl/datasets/fixed_poses_$DATASET_NAME" ]; then
-        echo "Directory '../instant-nsr-pl/datasets/fixed_poses_$DATASET_NAME' does not exist. Running surface normal detection"
-        
-        # Run test.py with $CASE_NAME as an argument (or modify as needed)
-        python estimation_surface_normal.py "$CASE_NAME"
-    else
-        echo "Using pre-computed fixed camera poses at '../instant-nsr-pl/datasets/fixed_poses_$DATASET_NAME'."
+    if [ "$SN_EST" = true ]; then
+        # run surface normal estimation
+        # first check if the directory exists
+        if [ ! -d "../instant-nsr-pl/datasets/fixed_poses_$DATASET_NAME" ]; then
+            echo "Directory '../instant-nsr-pl/datasets/fixed_poses_$DATASET_NAME' does not exist. Running surface normal detection"
+            
+            # Run test.py with $CASE_NAME as an argument (or modify as needed)
+            python estimation_surface_normal.py "$CASE_NAME"
+        else
+            echo "Using pre-computed fixed camera poses at '../instant-nsr-pl/datasets/fixed_poses_$DATASET_NAME'."
+        fi
     fi
-    
 
     # Run the second command
     python exp_runner.py --mode train --conf ./confs/wmask_ar.conf \
@@ -58,3 +66,4 @@ do
     # Return to the original directory after running the command
     cd - > /dev/null
 done
+echo "All cases processed successfully."
